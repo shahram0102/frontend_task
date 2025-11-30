03ec2457f3b1615778912513482b4a8a9d5686b9
shahram0102 <shahramkhaleh2001@gmail.com>
2025-12-01 02:09:43 +0330

    feat(types): add product, response, and review type definitions
    Introduce new TypeScript interfaces for product, responses, and reviews. The IProduct interface defines various properties relevant to product details, IResData provides a structure for paginated API responses, and IReview describes the format of customer reviews. These additions aim to enhance type safety and code clarity in handling product-related data.

---
+++ src/types/product.d.ts
@@ -0,0 +1,33 @@
+interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: IReview[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: Date;
+    updatedAt: Date;
+    barcode: string;
+    qrCode: string;
+  };
+  images: string[];
+  thumbnail: string;
+}
---
+++ src/types/review.d.ts
@@ -0,0 +1,7 @@
+interface IReview {
+  rating: number;
+  comment: string;
+  date: Date;
+  reviewerName: string;
+  reviewerEmail: string;
+}
