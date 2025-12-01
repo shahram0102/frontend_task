829c777bda777338b650afb59fb6c2c198cd99cd
shahram0102 <shahramkhaleh2001@gmail.com>
2025-12-01 03:55:24 +0330

    feat(cart): add StickyCart component to HomePage
    Introduced a new StickyCart component that shows the total quantity and price of items, encouraging users to complete their purchase. The component is integrated into the HomePage, dynamically appearing when there are items in the cart. This enhancement improves user experience by providing quick access to cart details while browsing products.

---
+++ src/app/_components/Home/StickyCart/StickyCart.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import useStore from "@/store/index.store";
+import { Button } from "../../ui/button";
+import { motion, AnimatePresence } from "framer-motion";
+import { getFinalPrice } from "@/utils/get-final-price";
+
+export default function StickyCart() {
+  const products = useStore.use.products();
+
+  const totalQuantity = products.reduce(
+    (acc, product) => acc + product.quantity,
+    0,
+  );
+  const totalPrice = products.reduce(
+    (acc, product) => acc + getFinalPrice(product) * product.quantity,
+    0,
+  );
+
+  const hasItems = totalQuantity > 0;
+
+  return (
+    <AnimatePresence>
+      {hasItems && (
+        <motion.footer
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ type: "spring", stiffness: 200, damping: 18 }}
+          className="fixed right-0 bottom-6 left-0 z-50 mx-auto px-4"
+        >
+          <div className="bg-primary mx-auto flex max-w-[500px] items-center justify-between rounded-[10px] p-4 shadow-lg">
+            <div className="flex items-center gap-2">
+              <Button className="rounded-full" size="icon" variant="secondary">
+                <span className="font-bold">{totalQuantity}</span>
+              </Button>
+              <span className="text-sm font-bold text-white">تکمیل خرید</span>
+            </div>
+
+            <span className="font-bold text-white">
+              {totalPrice.toLocaleString("fa-IR")} تومان
+            </span>
+          </div>
+        </motion.footer>
+      )}
+    </AnimatePresence>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getProducts } from "@/api/product";
 import { getQueryClient } from "@/utils/get-query-client";
-import Products from "./_components/Products/Products";
+import Products from "./_components/Home/Products/Products";
+import StickyCart from "./_components/Home/StickyCart/StickyCart";
 
 export default async function HomePage() {
   const queryClient = getQueryClient();
@@ -13,6 +14,7 @@ export default async function HomePage() {
   return (
     <main className="mx-auto h-full max-w-[500px] p-4">
       <Products products={products} />
+      <StickyCart />
     </main>
   );
 }
